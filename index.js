3ec2d4e9bb0822b93e3c87a7e8c41b8a38309281
vanderkilu <vanderkiliku@gmail.com>
2019-07-12 09:22:39 +0000

    add image toggle effect

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
-
+//state
 const leftCtrl = document.querySelector('.carousel__controls-left')
 const rightCtrl =  document.querySelector('.carousel__controls-right')
 const imageElements = Array.from(document.querySelectorAll('.carousel__img'))
 const indicatorElements = Array.from(document.querySelectorAll('.indicator'))
+const bigImage = document.querySelector('.reason__images--big')
+const smallImages = Array.from(document.querySelectorAll('.reason__images--item'))
+let start = 1
 
-
-
+//attaching listeners
 leftCtrl.addEventListener('click', showSlide(-1))
 rightCtrl.addEventListener('click', showSlide(1))
 indicatorElements.forEach((indicator, i) =>  {
     indicator.addEventListener('click', currentSlide(i+1))
 }) 
+smallImages.forEach(s => s.addEventListener('click', replaceImage(s)))
 
-let start = 1
+
+function replaceImage(smallImage) {
+    return ()=> {
+        smallImages.forEach(s => s.classList.remove('clear'))
+        smallImage.classList.add('clear')
+        let imgSrc = smallImage.getAttribute('src')
+        bigImage.setAttribute('src', imgSrc) 
+    }
+}
 
 function showSlide(incrementor) {
     return () => slide(start += incrementor)
@@ -23,7 +34,6 @@ function currentSlide(index) {
 }
 
 function slide(incrementor) {
-
     if (incrementor > imageElements.length) start = 1
     if (incrementor < 1) start = imageElements.length
     imageElements.forEach((imageElement)=> {
